eb4b6fbdccc5124de354e179e5a9f1862d40fc59
furkanaltaca <furkanaltaca@gmail.com>
2020-08-23 01:10:49 +0300

    Datatable example config added.

--- jquery_crud_template.js
+++ jquery_crud_template.js
@@ -1,9 +1,4 @@
 $(document).ready(function () {
-  //ARRANGE
-  var dataTable=$('#exampleDataTable');
-  var example=$('#example');
-  //END ARRANGE
-  //-------------------------------------------------------------------------
   //URL
   var urlGetAll = "";
   var urlGet = "";
@@ -11,6 +6,43 @@ $(document).ready(function () {
   var urlUpdate = "";
   var urlDelete = "";
   //END URL
+  //-------------------------------------------------------------------------
+  //ARRANGE
+  var dataTable = $("#exampleDataTable");
+  var dataTableConfig = {
+    dom: "Bfrtip",
+    processing: true,
+    serverSide: true,
+    responsive: true,
+    pageLength: 15,
+    ajax: GetDataTableAjaxConfig(),
+    columns: [
+      { data: "image" },
+      { data: "title" },
+      { data: "category.name" },
+      { data: "hit" },
+      { data: "created_at" },
+      { data: "status" },
+      { data: "transactions" },
+    ],
+    columnDefs: [
+      {
+        targets: -1,
+        data: null,
+        defaultContent: "<button>Click!</button>",
+      },
+      {
+        targets: -1, //-1 meaning is from right
+        data: null,
+        defaultContent: "<i>Not set</i>",
+        render: function (data, type, row) {
+          return "<button>datatable button</button>";
+        },
+      },
+    ],
+  };
+  var example = $("#example");
+  //END ARRANGE
   //-------------------------------------------------------------------------
   //EVENT
   example.on("click", function () {
@@ -30,6 +62,15 @@ $(document).ready(function () {
   //END FUNCTIONS
   //-------------------------------------------------------------------------
   //AJAX FUNCTIONS
+  function GetDataTableAjaxConfig(params = {}){
+    return {
+      type: "GET",
+      url: urlGetAll,
+      data: params,
+      dataType: "json",
+    };
+  }
+
   function GetAll(params = {}) {
     return $.ajax({
       type: "GET",
